7c365746f50d932dec7c50f59a6db3ac9fbde13f
Nathan <145821197+eunathan098@users.noreply.github.com>
2025-05-13 13:23:33 -0300

    Adição de e-mail na tela de Contato da página

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,50 +1,57 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-
-// import CSS
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import './Contato.css';
 
-function Contato(){
-    return(
-        <>
-          <Header></Header>
-            <div className="container-content-contato">
-              <div className="container-form">
+function Contato() {
+  return (
+    <>
+      <Header />
+      <div className="container-content-contato">
+        <div className="container-form">
+          <div className="container-form-header">
+            <h1>Entre em contato</h1>
+          </div>
+          <form
+            action="https://formsubmit.co/devnathan2025@gmail.com"
+            method="POST"
+          >
+            <input type="hidden" name="_captcha" value="false" />
+            
+            <input type="hidden" name="_next" value="https://eunathan098.github.io/React-Portfolio-V1/" />
 
-                <div className="container-form-header">
-                  <h1> Entre em contato </h1>
-                </div>
-                
-                  <form action="">
-                    <label>
-                      <div className="name-input">
-                        <span> nome: </span>
-                      </div>
-                      <input type="text" placeholder="Digite o seu nome..."/>
-                    </label>
-
-                    <label>
-                    <div className="name-input">
-                        <span> email: </span>
-                      </div>
-                      <input type="email" placeholder="Digite o seu melhor e-mail..."/>
-                    </label>
-
-                    <label>
-                    <div className="name-input">
-                        <span> Deixe sua mensagem: </span>
-                      </div>
-                      <textarea>
-
-
-                      </textarea>
-                    </label>
-                  </form>
-                  <input type="submit" id="btn-enviar"/>
+            {/* Nome */}
+            <label htmlFor="nome">
+              <div className="name-input">
+                <span>Nome:</span>
               </div>
-            </div>
-          <Footer></Footer>
-        </>
-    )
+            </label>
+            <input type="text" id="nome" name="nome" placeholder="Digite o seu nome..." required/>
+
+            <label htmlFor="email">
+              <div className="name-input">
+                <span>Email:</span>
+              </div>
+            </label>
+            <input type="email" id="email" name="email" placeholder="Digite o seu melhor e-mail..." required/>
+
+            <label htmlFor="mensagem">
+              <div className="name-input">
+                <span>Deixe sua mensagem:</span>
+              </div>
+            </label>
+            <textarea id="mensagem" name="mensagem" placeholder="Digite sua mensagem..." required>
+
+
+            </textarea>
+            <button type="submit" id="btn-enviar">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
-export default Contato
+
+export default Contato;
